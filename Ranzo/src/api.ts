5cfe04fd20ca7bef578b16ab53b41bc7f092edbe
Solomon Owamani <owamani44@gmail.com>
2026-03-04 18:50:48 +0300

    form med fail

--- Ranzo/src/api.ts
+++ Ranzo/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Use an environment variable for the API Gateway URL
-const BASE_URL = import.meta.env.VITE_API_GATEWAY_URL || "http://localhost:8084";
+const BASE_URL = import.meta.env.VITE_API_GATEWAY_URL || "http://localhost:8082";
 
 export const myAxios = axios.create({
   baseURL: BASE_URL,
